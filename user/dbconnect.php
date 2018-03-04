3ed98905c40540df9e17460e187c043d2e64e057
Mihir Lad <mihirlad55@gmail.com>
2018-03-04 21:36:42 +0000

    Added connection details to dbconnect.php - Defined database name, database username, hostname, and password - Tested connection to DB with mysqli successfully

--- user/dbconnect.php
+++ user/dbconnect.php
@@ -1,4 +1,19 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "", "STE_login") or die("Error " . mysqli_error($con));
+//$con = mysqli_connect("localhost", "root", "", "STE_login") or die("Error " . mysqli_error($con));
+
+    define("DB_NAME", "users");
+    define("DB_USER", "AWS");
+    define("DB_HOST", "207.223.160.10");
+    define("DB_PASSWORD", "6Jy%J6uWsIXr*t@^");
+    
+    //connect to the sql databse
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    
+    //if connection fails, exit and send error message
+    if ($conn->connect_error) {
+    	die("Could not connect: " . $conn->connect_error);
+    }
+    else echo "Connection successful";
+
 ?>
