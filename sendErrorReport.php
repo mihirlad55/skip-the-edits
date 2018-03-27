007c4f4bc5d7a15ea06b11c6c8d70e2a8aad1484
Mihir Lad <mihirlad55@gmail.com>
2018-03-27 00:32:13 +0000

    Created PHP Script sendErrorReport.php for reporting errors - The script sends an error report with details such as the userID, User Agent, and screen dimensions to the company email - THe script also checks to make sure the user is logged in by checking the SESSION variables

---
+++ sendErrorReport.php
@@ -0,0 +1,18 @@
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        session_start();
+        $userID;
+        if ($_SESSION["isLoggedIn"])
+        {
+            $userID = $_SESSION["userID"];
+            session_write_close();
+        }
+        $email = "skiptheedits@gmail.com";
+        $content = $_POST["message"] . "\n userID: $userID\n User Agent: {$_POST['userAgent']}\n Screen Height: {$_POST['screenHeight']}\n Screen Width: {$_POST['screenWidth']}";
+        $subject = "Error Report";
+        
+        mail($email, $subject, $content, "noreply@skiptheedits.com");
+    }
+?>
