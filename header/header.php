1709d468b5a261582da3ae235473b67d6f3e671e
Shichao Jiang <xtornado47@hotmail.com>
2018-05-04 12:54:41 -0400

    Moved header html and css to separate files for use in multiple pages

---
+++ header/header.php
@@ -0,0 +1,15 @@
+<header id="header-main">
+    <img class="navbar-icon" src="../img/logo-nb.png" id="logo"/>
+    <a class="navbar-icon" id="iconAvatar" href="../..">
+        <img src="../img/avatar.png" />
+    </a>
+    <a class="navbar-icon" id="iconNotification">
+        <img src="../img/notification.png"/>
+    </a>
+    <a class="navbar-icon" id="iconUpload" href="../upload/upload.html">
+        <img src="../img/upload.png"/>
+    <a/>
+    <a class="navbar-icon" id="iconHome" href="../home/home.html">
+        <img src="../img/home.png"/>
+    </a>
+</header>
