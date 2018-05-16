64e37745195d93ff47a6ddd34012923e5c9971bd
Shichao Jiang <xtornado47@hotmail.com>
2018-05-16 12:24:22 -0400

    - Updated logincheck.php to contain modal display code

--- logincheck.php
+++ logincheck.php
@@ -3,4 +3,9 @@
         session_destroy();
         header('location: login.html');
     }
+
+    if (isset($_SESSION['msg'])) {
+        include_once '../modal.php';
+        unset($_SESISON['msg']);
+    }
 ?>
