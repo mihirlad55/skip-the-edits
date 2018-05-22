81792c88a1ba558e355a137b28c41b44add9304c
Mihir Lad <mihirlad55@gmail.com>
2018-05-22 03:22:31 +0000

    Fixed URLs for PHP Script References

--- home/loadessays.php
+++ home/loadessays.php
@@ -1,5 +1,5 @@
 <?php foreach ($essays as $essay): ?>
-    <a class="content tile" href="../view/view.html?id=<?php echo $essay['id'] ?>">
+    <a class="content tile" href="../edit?id=<?php echo $essay['id'] ?>">
 
         <p><?php echo $essay['title'] ?></p>
     </a>
