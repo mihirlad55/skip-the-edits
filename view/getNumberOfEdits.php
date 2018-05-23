44d2ac67181c69684dc3fece0d1767efdfc70a42
Mihir Lad <mihirlad55@gmail.com>
2018-05-23 04:13:55 +0000

    Created PHP Script in View Directory for Returning Number of Edits Made on Essay with a Given Id

---
+++ view/getNumberOfEdits.php
@@ -0,0 +1,14 @@
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] != "GET") die("Not a GET Request!");
+    
+    require_once("../php/dbconnect.php");
+    
+    $essayId = $conn->real_escape_string($_GET["id"]);
+    
+    $query = "SELECT COUNT(*) AS numberOfEdits FROM Edits WHERE essayId=${essayId}";
+    
+    if ( !($result = $conn->query($query)) ) die($conn->error());
+    
+    echo($result->fetch_assoc()["numberOfEdits"]);
+?>
