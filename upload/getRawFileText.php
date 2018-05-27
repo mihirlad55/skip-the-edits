3f702ee9ffda6894205b2098ebda0eb146f33108
Mihir Lad <mihirlad55@gmail.com>
2018-05-27 19:14:42 +0000

    Changed upload webpage to contain separate preview and upload webpages - Added getRawFileText.php script to get plain text from an uploaded word document or text file - Created basic HTML/styling for preview webpage - Removed unnessary leftover styling from upload webpage - Changed Form action for upload.html

---
+++ upload/getRawFileText.php
@@ -0,0 +1,27 @@
+<?php
+
+    include_once("docConvert.php");
+        
+    session_start();
+    
+    /*if (!$_SESSION["isLoggedIn"]) {
+        session_abort();
+        die("Not Logged In.");
+    }
+    else if ($_SERVER["REQUEST_METHOD"] != "POST")
+    {
+        session_abort();
+        die("Not a POST request.");
+    }*/
+
+    $info = pathinfo($_FILES['file']['name']);
+    
+    if (!($_FILES['file']['type'] == "text/plain" || $info['extension'] == "docx" || $info['extension'] == 'doc')) die("Error: Not a supported file format.");
+
+    if ( $info['extension'] == "docx" || $info['extension'] == 'doc')
+    {
+        if ( $info['extension'] == "docx") echo(readDocx($_FILES['file']['tmp_name']));
+        else echo(readDoc($_FILES['file']['tmp_name']));
+    }
+    else echo(file_get_contents($_FILES['file']['tmp_name']));
+?>
