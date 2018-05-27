675027bb169c022f6f341278f206a0c0b1197aa6
Mihir Lad <mihirlad55@gmail.com>
2018-05-27 18:11:00 -0400

    Added fail-safe session_start to logincheck.php

--- php/logincheck.php
+++ php/logincheck.php
@@ -1,4 +1,9 @@
 <?php
+
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
     if (!isset($_SESSION['user'])) {
         session_destroy();
         header('location: ../login/');
@@ -8,4 +13,6 @@
         include_once '../modal.php';
         unset($_SESSION['msg']);
     }
+
+    session_abort();
 ?>
