21c64bf73b53e5d638ae5e69df4129f7eca069c1
Shichao Jiang <xtornado47@hotmail.com>
2018-05-28 20:17:58 -0400

    - Login now uses javascript to check password length and matching - Fixed modal not showing on home - Registering now requires you to login after - Added modal after registering - Fixed being able to access register page after failed first step

--- login/login.js
+++ login/login.js
@@ -1,7 +1,39 @@
+var error1 = true;
+var error2 = true;
+var error3 = true;
+
+function displayerror(x, y) {
+    if (x === true) {
+        document.getElementById(y).style.display = 'block';
+    }
+}
+
 function checkemail() {
 
 }
 
-function checkpass() {
+function checklength(x) {
+    if (x.value.length < 6 && x.value.length > 0) {
+        error2 = true;
+    } else {
+        document.getElementById('error2').style.display = 'none';
+        error2 = false;
+    }
+}
 
+function checkmatch(x) {
+    if (document.getElementById("cpassword").value != document.getElementById("password").value) {
+        error3 = true;
+    } else {
+        document.getElementById('error3').style.display = 'none';
+        error3 = false;
+    }
+}
+
+function checkall() {
+    if ((error2 === true) || (error3 === true)) {
+        document.getElementById('next').disabled = true;
+    } else {
+        document.getElementById('next').disabled = false;
+    }
 }
