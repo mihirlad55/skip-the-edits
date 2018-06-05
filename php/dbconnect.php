629ca33970578b464d614914b302459923f37b67
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 16:50:13 -0400

    Linted and commented dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,16 +1,15 @@
 <?php
-//connect to mysql database
 
-    define("DB_DATABASE", "skiptheedits");
-    define("DB_USER", "AWS");
-    define("DB_HOST", "207.223.160.10");
-    define("DB_PASSWORD", "6Jy%J6uWsIXr*t@^");
-    
-    //connect to the sql databse
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-    
-    //if connection fails, exit and send error message
-    if ($conn->connect_error) {
-    	die("Could not connect: " . $conn->connect_error);
-    }
-?>
+// Define some constants for the database connection
+define("DB_DATABASE", "skiptheedits");
+define("DB_USER", "AWS");
+define("DB_HOST", "207.223.160.10");
+define("DB_PASSWORD", "6Jy%J6uWsIXr*t@^");
+
+// Connect to the MySQL database
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+// If connection fails, kill script, and print error message
+if ($conn->connect_error) {
+    die("Could not connect: " . $conn->connect_error);
+}
