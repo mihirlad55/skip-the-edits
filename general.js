110cf768e1a7a40613786c0f0500d9ccd827091d
Mihir Lad <mihirlad55@gmail.com>
2018-06-04 22:27:54 -0400

    Linted general.js

--- general.js
+++ general.js
@@ -1,42 +1,66 @@
-function getCookie(name)
-{
-	var cookieArray = document.cookie.toString().split(';');
-	for (var index = 0; index < cookieArray.length; index++)
-	{
-		if (cookieArray[index].indexOf(name) != -1)
-		{
-			return cookieArray[index].replace(name + "=", "").trim().replace(/\+/g, " ");
-		}
-	}
+/* exported getCookie formatDate getGetVariable */
+/**
+ * Get Cookie Data
+ * @param {String} name Name of cookie
+ * @return {String} Value of cookie
+ */
+function getCookie(name) {
+    let cookieArray = document.cookie.toString().split(';');
+    for (let index = 0; index < cookieArray.length; index++) {
+        if (cookieArray[index].indexOf(name) != -1) {
+            return cookieArray[index]
+                .replace(name + '=', '').trim().replace(/\+/g, ' ');
+        }
+    }
 }
 
-function formatDate(date, format)
-{
-	var monthList = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-	var dayList = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
-	
-	var hour = date.getHours();
-	var period = "AM";
-	
-	if (hour > 11)
-	{
-		hour = hour - 12;
-		period = "PM";
-	}
-		
-	return format.replace("%month", monthList[date.getMonth()]).replace("%day", dayList[date.getDay()]).replace("%year", date.getFullYear()).replace("%hour", hour).replace("%period", period).replace("%minute", (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()).replace("%second", date.getSeconds()).replace("%date", date.getDate());
+/**
+ * Format Date Object into String
+ * @param {Date} date Date Object
+ * @param {String} format Format string with token
+ * @return {String} Formatted Date
+ */
+function formatDate(date, format) {
+    let monthList = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+    let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+        'Friday', 'Saturday'];
+
+    let hour = date.getHours();
+    let period = 'AM';
+
+    if (hour > 11) {
+        hour = hour - 12;
+        period = 'PM';
+    }
+
+    return format
+        .replace('%month', monthList[date.getMonth()])
+        .replace('%day', dayList[date.getDay()])
+        .replace('%year', date.getFullYear())
+        .replace('%hour', hour)
+        .replace('%period', period)
+        .replace('%minute', (
+            date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
+        )
+        .replace('%second', date.getSeconds())
+        .replace('%date', date.getDate());
 }
 
-function getGetVariable (variable)
-{
-	var getUri = document.location.href.substr(document.location.href.indexOf("?") + 1);
-	var variables = getUri.split("&");
-	
-	for (var i = 0; i < variables.length; i++)
-	{
-		if (variables[i].indexOf(variable) != -1)
-		{
-			return variables[i].replace(variable + "=", "");
-		}
-	}
+
+/**
+ * Get Value of GET Request Variable
+ * @param {String} variable Name of GET Variable
+ * @return {String} Value of GET Request Variable
+ */
+function getGetVariable(variable) {
+    let getUri = document.location.href
+        .substr(document.location.href.indexOf('?') + 1);
+    let variables = getUri.split('&');
+
+    for (let i = 0; i < variables.length; i++) {
+        if (variables[i].indexOf(variable) != -1) {
+            return variables[i].replace(variable + '=', '');
+        }
+    }
 }
