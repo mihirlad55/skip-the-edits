8f97ceeaf219d50e97afab71f334c6fd85aedd1c
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 17:52:29 -0400

    Commented profile/edit/edit.js

--- profile/edit/edit.js
+++ profile/edit/edit.js
@@ -5,6 +5,7 @@
  * Load User Info into Inputs/Textfields
  */
 function loadInfo() {
+    // Update textboxes with current profile details
     document.getElementById('txtFirstName').value = info.firstName;
     document.getElementById('txtLastName').value = info.lastName;
     document.getElementById('txtProfession').value = info.profession;
@@ -21,11 +22,16 @@ function loadInfo() {
  * @param {Event} event Event Object from input file onchange
  */
 function refreshPreview(event) {
+    // Get filename of uploaded file
     document.getElementById('inputFilePath').value = document.
         getElementById('filePicker').value.substr(12);
 
+    // Get reference to img imagePreview
     let imagePreview = document.getElementById('imagePreview');
+
+    // If a file has been uploaded...
     if (event.target.files[0]) {
+        // Set the src of the img element to the Object URL of uploaded file
         imagePreview.src = URL.createObjectURL(event.target.files[0]);
     }
 }
