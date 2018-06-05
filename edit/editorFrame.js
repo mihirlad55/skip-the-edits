28f8c3f646bd3b43ecd248f0c4f0b6e1eb0a91c1
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 17:49:08 -0400

    Commented editorFrame.js

--- edit/editorFrame.js
+++ edit/editorFrame.js
@@ -5,9 +5,14 @@
  * @param {Number} id ID of Change
  */
 function showComment(id) {
+    // Set changeEl to reference of change card
     let changeEl = document.getElementById('change' + id.toString());
+    // Set commentEl to reference of change's corresponding comment element
     let commentEl = changeEl.getElementsByClassName('change-comment')[0];
+    // Set className of comment element to show comment
     commentEl.className = 'change-comment';
+
+    // Reorganize change cards
     parent.sortChanges();
 }
 
@@ -17,8 +22,13 @@ function showComment(id) {
  * @param {Number} id ID of Change
  */
 function hideComment(id) {
+    // Set changeEl to reference of change card
     let changeEl = document.getElementById('change' + id.toString());
+    // Set commentEl to reference of change's corresponding comment element
     let commentEl = changeEl.getElementsByClassName('change-comment')[0];
+    // Set className of comment element to hide comment
     commentEl.className = 'change-comment hidden';
+
+    // Reorganize change cards
     parent.sortChanges();
 }
