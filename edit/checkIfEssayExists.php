9fbc72d024636671469075775facc2dc2f4de1a9
Mihir Lad <mihirlad55@gmail.com>
2018-06-04 23:59:03 -0400

    Sniffed checkIfEssayExists.php

--- edit/checkIfEssayExists.php
+++ edit/checkIfEssayExists.php
@@ -1,27 +1,30 @@
 <?php
 
-    require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
+require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
     
-    if ($_SERVER["REQUEST_METHOD"] != "GET") die("Not a GET Request!");
-    
-    require_once("../php/dbconnect.php");
-    
-    $essayId = -1;
-    if (isset($_GET["id"])) {
-        $essayId = $conn->real_escape_string($_GET["id"]);
-    }
+if ($_SERVER["REQUEST_METHOD"] != "GET") {
+    die("Not a GET Request!");
+}
 
-    $query = "SELECT COUNT(*) AS doesEssayExist FROM Essays WHERE id=${essayId}";
+require_once("../php/dbconnect.php");
+
+$essayId = -1;
+if (isset($_GET["id"])) {
+    $essayId = $conn->real_escape_string($_GET["id"]);
+}
+
+$query = "SELECT COUNT(*) AS doesEssayExist FROM Essays WHERE id=${essayId}";
     
-    $result;
-    if ( !($result = $conn->query($query)) ) die($conn->error);
-    
-    if ($result->fetch_assoc()['doesEssayExist'] == 0) {
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-        $_SESSION['msg'] = 'Essay does not exist!';
-        session_write_close();
-        header('Location: /home');
+$result;
+if (!($result = $conn->query($query))) {
+    die($conn->error);
+}
+
+if ($result->fetch_assoc()['doesEssayExist'] == 0) {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
     }
-?>
+    $_SESSION['msg'] = 'Essay does not exist!';
+    session_write_close();
+    header('Location: /home');
+}
