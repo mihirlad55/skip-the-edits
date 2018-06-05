023d71c1b7802c0a8802f4db0b5ddce22a492a29
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 17:59:01 -0400

    Linted and commented getPrivateProfileInfo.php

--- profile/edit/getPrivateProfileInfo.php
+++ profile/edit/getPrivateProfileInfo.php
@@ -1,24 +1,51 @@
 <?php
 
-    require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
+// Execute logincheck.php to check if user is logged in
+require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
 
-    if ($_SERVER["REQUEST_METHOD"] != "GET") die("Not a GET request.");
+// Check if REQUEST_METHOD is GET
+if ($_SERVER["REQUEST_METHOD"] != "GET") {
+    // Kill script and print error
+    die("Not a GET request.");
+}
 
-    require_once("${_SERVER['DOCUMENT_ROOT']}/php/dbconnect.php");
+// Connect to the MySQL database
+require_once("${_SERVER['DOCUMENT_ROOT']}/php/dbconnect.php");
 
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    $userId = $_SESSION["user"]["id"];
-    session_abort();
+// If a session has not been started, start a session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-    $queryString = "SELECT id AS userId, firstName, lastName, profession, location, website, twitterHandle, bio FROM Users WHERE id = ${userId}";
-                    	
-    if (!($result = $conn->query($queryString)))
-    {
-        session_abort();
-        die($conn->error);
-    }
-    
-    echo(json_encode($result->fetch_assoc()));
-?>
+// Get ID of logged on user from $_SESSION variable
+$userId = $_SESSION["user"]["id"];
+
+// Stop the session
+session_abort();
+
+/*
+ * Create query that gets a row of the userId, firstName, lastName, profession,
+ * location, website, twitterHandle, and bio of user with a certain id
+ */
+$queryString = "SELECT
+                    id AS userId,
+                    firstName,
+                    lastName,
+                    profession,
+                    location,
+                    website,
+                    twitterHandle,
+                    bio
+                FROM
+                    Users
+                WHERE
+                    id = ${userId}";
+
+// Try querying the database and assigning the result to $result
+if (!($result = $conn->query($queryString))) {
+    // If error, kill script and print error
+    die($conn->error);
+}
+
+// Print the $result associative row as a JSON string
+echo(json_encode($result->fetch_assoc()));
