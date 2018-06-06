723892709e00e935eea0160b678532958223128e
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 23:08:25 -0400

    Commented and linted rateEditMessage.php

--- view/rateEditMessage.php
+++ view/rateEditMessage.php
@@ -1,25 +1,72 @@
 <?php
 
-    require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
+// Execute logincheck.php to check if user is logged in
+require_once("${_SERVER['DOCUMENT_ROOT']}/php/logincheck.php");
 
-    if ($_SERVER['REQUEST_METHOD'] != "POST") die("Not a POST Request");
 
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    $userId = $_SESSION['user']['id'];
-    session_abort();
+// Check if REQUEST_METHOD is POST
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    // Kill script and print error
+    die("Not a POST Request!");
+}
 
-    require_once("${_SERVER['DOCUMENT_ROOT']}/php/dbconnect.php");
-    
-    $rating = $conn->real_escape_string($_POST['rating']);
-    $editMessageId = $conn->real_escape_string($_POST['editMessageId']);
 
-    $query = "DELETE FROM MessageRatings WHERE userId = ${userId} AND messageId = ${editMessageId} LIMIT 1";
+// If a session has not been started, start a session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-    if (!($conn->query($query))) die($conn->error);
+// Get ID of logged on user from $_SESSION variable
+$userId = $_SESSION['user']['id'];
+// Stop the session
+session_abort();
 
-    $query = "INSERT INTO MessageRatings (userId, messageId, rating) VALUES (${userId}, ${editMessageId}, '${rating}')";
+// Connect to the MySQL database
+require_once("${_SERVER['DOCUMENT_ROOT']}/php/dbconnect.php");
 
-    if (!($conn->query($query))) die($conn->error);
-?>
+// Escape the special characters and set the POST variables to global variables
+$rating = $conn->real_escape_string($_POST['rating']);
+$editMessageId = $conn->real_escape_string($_POST['editMessageId']);
+
+/*
+ * Create a MySQL query string that deletes a row from the MessageRatings table
+ * where the userId matches the current logged in user's id and the messageId
+ * equals the given message ID
+ */
+$query = "
+    DELETE FROM
+        MessageRatings
+    WHERE
+        userId = ${userId}
+    AND
+        messageId = ${editMessageId}
+    LIMIT 1";
+
+// Try querying the database
+if (!($conn->query($query))) {
+    // If error, kill script and print error
+    die($conn->error);
+}
+
+/*
+ * Create a MySQL query string that inserts a new row into the MessageRatings
+ * table with the userId, editMessageId, and the user's rating
+ */
+$query = "
+    INSERT INTO
+        MessageRatings (
+            userId,
+            messageId,
+            rating
+        )
+    VALUES (
+        ${userId},
+        ${editMessageId},
+        '${rating}'
+    )";
+
+// Try querying the database
+if (!($conn->query($query))) {
+    // If error, kill script and print error
+    die($conn->error);
+}
