061c0026143ae883d6487c259e23a0bff805f51d
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 21:40:27 -0400

    Commented and linted getRawFileText.php

--- upload/preview/getRawFileText.php
+++ upload/preview/getRawFileText.php
@@ -1,17 +1,36 @@
 <?php
 
-    include_once("../docConvert.php");
-        
-    if ($_SERVER["REQUEST_METHOD"] != "POST") die("Not a POST request.");
- 
-    $info = pathinfo($_FILES['file']['name']);
-    
-    if (!($_FILES['file']['type'] == "text/plain" || $info['extension'] == "docx" || $info['extension'] == 'doc')) die("Error: Not a supported file format.");
+// Include the code from docConvert.php
+include_once("../docConvert.php");
 
-    if ( $info['extension'] == "docx" || $info['extension'] == 'doc')
-    {
-        if ( $info['extension'] == "docx") echo(readDocx($_FILES['file']['tmp_name']));
-        else echo(readDoc($_FILES['file']['tmp_name']));
+// Check if REQUEST_METHOD is POST
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    // Kill script and print error
+    die("Not a POST request.");
+}
+
+
+// Get file info from the uploaded file
+$info = pathinfo($_FILES['file']['name']);
+
+// Check if the file is plain text or a word document; print error if it isn't
+if (!($_FILES['file']['type'] == "text/plain" || $info['extension'] == "docx"
+    || $info['extension'] == 'doc')) {
+        die("Error: Not a supported file format.");
+}
+
+
+// If the extension of the file is .docx or .doc
+if ($info['extension'] == "docx" || $info['extension'] == 'doc') {
+    // If the extension of the file is .docx
+    if ($info['extension'] == "docx") {
+        // Print out the text within the .docx file
+        echo(readDocx($_FILES['file']['tmp_name']));
+    } else {
+        // Print out the text within the .doc file
+        echo(readDoc($_FILES['file']['tmp_name']));
     }
-    else echo(file_get_contents($_FILES['file']['tmp_name']));
-?>
+} else {
+    // If the extension isn't .docx or .doc, print the contents of the file
+    echo(file_get_contents($_FILES['file']['tmp_name']));
+}
