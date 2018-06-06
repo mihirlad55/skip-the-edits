c197d59a33e8b4d401690c17679b9c7884339b1c
Mihir Lad <mihirlad55@gmail.com>
2018-06-05 21:34:31 -0400

    Commented preview.js

--- upload/preview/preview.js
+++ upload/preview/preview.js
@@ -6,11 +6,16 @@
  * @param {Event} event Event Object for onsubmit event
  */
 function validateForm(event) {
+    // Set richPage to a reference of the page
     let richPage = document.getElementById('page');
+    // Set the innerHTML of txtPage to the innerHTML of richPage
     document.getElementById('txtPage').innerHTML = richPage.innerHTML;
 
+    // If removing trailing/leading spaces from richPage results in empty text
     if (richPage.innerHTML.trim() == '') {
+        // Notify the user
         alert('Cannot submit a blank essay! Please enter your essay!');
+        // Prevent the automatic form submission
         event.preventDefault();
     }
 }
